88dd5528a8e548b023336dec053b186a38c61b2a
Killko Hon <killko@qq.com>
2012-10-25 15:11:57 +0800

    顶级项目

---
+++ project/Backend/UserAccount/src/main/java/com/ponder/ecommerce/Backend/UserAccount/Activator.java
@@ -0,0 +1,26 @@
+/*
+ *
+ *
+ */
+package com.ponder.ecommerce.Backend.UserAccount;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ *
+ * @author KillkoHon
+ */
+public class Activator implements BundleActivator{
+
+    @Override
+    public void start(BundleContext context) throws Exception {
+        //暂时没内容
+    }
+
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        //暂时没内容
+    }
+
+}
