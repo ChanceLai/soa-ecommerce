c1da7c53f9820005065186ab1e02c22fb45c0d75
Killko Hon <killko@qq.com>
2012-12-22 14:15:30 +0800

    Create

--- examples/Tutorials/app01/AddModule/src/main/java/com/ponder/tutorials/app01/AddModule/Activator.java
+++ examples/Tutorials/app01/AddModule/src/main/java/com/ponder/tutorials/app01/AddModule/Activator.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.ponder.tutorials.app01.AddModule;
 
 import org.osgi.framework.BundleActivator;
@@ -15,12 +11,12 @@ public class Activator implements BundleActivator{
 
     @Override
     public void start(BundleContext context) throws Exception {
-
+        System.out.println("AddModule启动！");
     }
 
     @Override
     public void stop(BundleContext context) throws Exception {
-        
+        System.out.println("AddModule停止！");
     }
 
 }
