ac83362b2c099e191f0475d6d78d887aefe77355
Killko Hon <killko@qq.com>
2012-12-22 13:57:57 +0800

    Create

---
+++ examples/Tutorials/app01/SubModule/src/main/java/com/ponder/tutorials/app01/SubModule/Activator.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ponder.tutorials.app01.SubModule;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Activator implements BundleActivator{
+
+    @Override
+    public void start(BundleContext context) throws Exception {
+
+    }
+
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        
+    }
+
+}
